92e48068e4741ca8209fef38a32574a70b91643e
Kablav <98692887+Kablav@users.noreply.github.com>
2022-01-31 22:38:36 +0300

    30 Ocak ders sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void ekleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.SoyAd + " müşterisi sisteme eklendi.");
+            Console.WriteLine("-----------------------------");
+        }
+
+        public void listeleme(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.SoyAd);
+            }
+            Console.WriteLine("-----------------------------");
+        }
+
+        public void silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.SoyAd + " müşterisi sistemden silindi.");
+
+        }
+    }
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123;
+            musteri1.Ad = "Percy";
+            musteri1.SoyAd = "Jackson";
+            musteri1.TelNo = 321;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 456;
+            musteri2.Ad = "Sally";
+            musteri2.SoyAd = "Jackson";
+            musteri2.TelNo = 654;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 789;
+            musteri3.Ad = "Annabeth";
+            musteri3.SoyAd = "Chase";
+            musteri3.TelNo = 987;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager mustermanager = new MusteriManager();
+            mustermanager.ekleme(musteri1);
+            mustermanager.listeleme(musteriler);
+            mustermanager.silme(musteri3);
+
+        }
+    }
+}
